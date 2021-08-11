8e588e56ae9cf0e00f549a67d3cf73e44e5dc4ab
Pierre enoc <pierre.enoc@gympass.com>
2021-08-11 18:08:30 -0300

    :sparkles: add course entity

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course(name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp Html', 'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg', 'https://cdn.pixabay.com/photo/2020/05/31/09/40/online-course-5242018_1280.jpg');
