555d0a2bb9f4e6545145547dd076deab8175fc3e
Pierre enoc <pierre.enoc@gympass.com>
2021-08-12 12:54:22 -0300

    :sparkles: add section entity

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Section.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Section.java
@@ -1,8 +1,6 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,11 +11,11 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.devsuperior.dslearnbds.entities.enums.ResourceType;
+import com.devsuperior.dslearnbds.entities.enums.DeliverStatus;
 
 @Entity
-@Table(name = "tb_resource")
-public class Resource implements Serializable {
+@Table(name = "tb_section")
+public class Section implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -27,103 +25,93 @@ public class Resource implements Serializable {
 	private String description;
 	private Integer position;
 	private String imgUri;
-	private ResourceType type;
+	
 	
 	@ManyToOne
-	@JoinColumn(name = "offer_id")
-	private Offer offer;
+	@JoinColumn(name = "resource_id")
+	private Resource resource;
 	
-	@OneToMany(mappedBy = "resource")
-	List<Section> sections = new ArrayList<>();
+	@ManyToOne
+	@JoinColumn(name = "prerequisite_id")
+	private Section prerequisite;
 	
-	
-	public Resource() {
+	public Section() {
 		
 	}
 
-
-	public Resource(Long id, String title, String description, Integer position, String imgUri, ResourceType type,
-			Offer offer) {
+	public Section(Long id, String title, String description, Integer position, String imgUri, DeliverStatus status,
+			Resource resource) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.description = description;
 		this.position = position;
 		this.imgUri = imgUri;
-		this.type = type;
-		this.offer = offer;
+		this.resource = resource;
+	}
+	
+	
+	public Section getPrerequisite() {
+		return prerequisite;
 	}
 
+	public void setPrerequisite(Section prerequisite) {
+		this.prerequisite = prerequisite;
+	}
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public String getTitle() {
 		return title;
 	}
 
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 
 	public String getDescription() {
 		return description;
 	}
 
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
 
 	public Integer getPosition() {
 		return position;
 	}
 
-
 	public void setPosition(Integer position) {
 		this.position = position;
 	}
-
 
 	public String getImgUri() {
 		return imgUri;
 	}
 
-
 	public void setImgUri(String imgUri) {
 		this.imgUri = imgUri;
 	}
 
-
-	public ResourceType getType() {
-		return type;
+	public Resource getResource() {
+		return resource;
 	}
 
-
-	public void setType(ResourceType type) {
-		this.type = type;
+	public void setResource(Resource resource) {
+		this.resource = resource;
 	}
 
-
-	public Offer getOffer() {
-		return offer;
+	@Override
+	public String toString() {
+		return "Section [id=" + id + ", title=" + title + ", description=" + description + ", position=" + position
+				+ ", imgUri=" + imgUri + ",  resource=" + resource + "]";
 	}
-
-
-	public void setOffer(Offer offer) {
-		this.offer = offer;
-	}
-
 
 	@Override
 	public int hashCode() {
@@ -133,7 +121,6 @@ public class Resource implements Serializable {
 		return result;
 	}
 
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -142,7 +129,7 @@ public class Resource implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Resource other = (Resource) obj;
+		Section other = (Section) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -150,12 +137,5 @@ public class Resource implements Serializable {
 			return false;
 		return true;
 	}
-
-	@Override
-	public String toString() {
-		return "Resource [id=" + id + ", title=" + title + ", description=" + description + ", position=" + position
-				+ ", imgUri=" + imgUri + ", type=" + type + ", offer=" + offer + "]";
-	}
-	
 	
 }
