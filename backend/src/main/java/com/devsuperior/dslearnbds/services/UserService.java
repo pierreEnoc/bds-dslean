de07a8105805fca90e106a1589fd3ffb781e19c3
Pierre enoc <pierre.enoc@gympass.com>
2021-08-16 13:19:33 -0300

    :sparkles: add user byID

--- backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dslearnbds.services;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +10,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dslearnbds.dto.UserDTO;
 import com.devsuperior.dslearnbds.entities.User;
 import com.devsuperior.dslearnbds.repositories.UserRepository;
+import com.devsuperior.dslearnbds.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -29,6 +33,13 @@ public class UserService implements UserDetailsService {
 			}
 			logger.info("User found: " + username);
 			return user;
+		}
+		
+		public UserDTO findById(Long id) {
+			
+			Optional<User> obj = repository.findById(id);
+			User entity = obj.orElseThrow(()-> new ResourceNotFoundException("Entity not found"));
+			return new UserDTO(entity);
 		}
 	}
 	
